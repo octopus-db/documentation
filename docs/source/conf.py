70c7c0325b9044dccd0f288ef77c1f9c3a1e4392
Henry Munack <59975737+hmunack@users.noreply.github.com>
2023-03-03 19:02:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OCTOPUS database'
+copyright = '2023, Henry'
+author = 'Henry'
 
 release = '0.1'
 version = '0.1.0'
