a4908227948522a0808063ccbbf459e1c74d93d3
Henry Munack <59975737+hmunack@users.noreply.github.com>
2023-04-29 12:05:59 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,15 +28,15 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
-html_theme_options = {
-    'html_logo': './img/logo.png'
-}
-
-# html_static_path = ['_static']
-
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
