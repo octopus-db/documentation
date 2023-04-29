e0b61e921d0604bfc5625b8d7bf4fee023e94943
Henry Munack <henrymunack@gmail.com>
2023-04-29 11:46:07 +0200

    ...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,15 +29,7 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 html_theme_options = {
-#    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-#    'analytics_anonymize_ip': False,
-#    'logo_only': False,
-#    'display_version': True,
-#    'prev_next_buttons_location': 'bottom',
-#    'style_external_links': False,
-#    'vcs_pageview_mode': '',
-#    'style_nav_header_background': 'white',
-     'html_logo': './img/logo.png'
+    'html_logo': './img/logo.png'
 }
 
 # html_static_path = ['_static']
