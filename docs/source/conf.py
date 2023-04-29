37fbe3df399e8a1d0ab82f31cdd419f16485e6fa
Henry Munack <henrymunack@gmail.com>
2023-04-29 13:19:16 +0200

    ...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,10 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "logo.png"
-#html_theme_options = {
-#    'logo_only': True,
-#    'display_version': False,
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
