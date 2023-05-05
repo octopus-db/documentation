a8e2ed5a3da181dc10188158a5a208830fdd5ed6
Henry Munack <henrymunack@gmail.com>
2023-05-05 10:48:42 +0200

    undo alabaster

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "logo_lscape.png"
 html_theme_options = {
